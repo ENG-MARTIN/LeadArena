7e6cc6d8b4a83b0f9b061c5ec9b6d7767c2c2baa
auresan-international <auresan.intl.ltd@gmaail.com>
2026-09-01 10:45:54 +0300

    order id and Styles for admin

--- auresancrm/settings.py
+++ auresancrm/settings.py
@@ -151,9 +151,6 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
